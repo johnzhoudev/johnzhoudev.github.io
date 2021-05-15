79866dbefc74314244c7a3a72429e60f9bb63f22
John Zhou <john2001@live.ca>
2021-05-14 23:12:30 -0400

    Add background image and greeting - yet to style for mobile

--- johnzhoudev-website/src/app/app.component.ts
+++ johnzhoudev-website/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello.';
+  title = 'Good Evening.';
 
   onSubheadingMouseEnter(): void {
     this.toggleMenuVisibility();
